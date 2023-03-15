e5750e6858eb44dc62c8be962c6cfeedda5f17f7
pankhuri813 <112609769+pankhuri813@users.noreply.github.com>
2023-03-15 15:17:46 +0530

    Update files and add new ones

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './Homepage';
+import Homemain from './Homemain';
+import Categories from './Categories';
+import Kathak from './Kathak';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+<Router>
+  <Routes>
+    <Route path ="/" element={<Homepage/>}/>
+    <Route path="/homemain" element={ <Homemain/> }/>
+    <Route path="/categorypage" element={<Categories/>}/>
+    <Route path ="/kathak" element={<Kathak/>}/>
+  </Routes>
+</Router>
+
     </div>
+
   );
 }
 
---
+++ src/Homemain.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Components/Navbar';
+import Carousel from './Components/Carousel'
+import About from './Components/About'
+import Cards from './Components/Cards'
+import brr from './images/brr.PNG'
+
+// import Homepage from './Homepage.js';
+
+function Homemain() {
+  return (
+    <div>
+
+
+<Navbar> </Navbar>
+
+<Carousel> </Carousel>
+
+<About> </About>
+
+<Cards> </Cards>
+    </div>
+  )
+}
+
+export default Homemain
